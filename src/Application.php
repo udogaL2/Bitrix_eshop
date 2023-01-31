4b728e0c068c3d32c9f14289f4f8a71078a0ecfc
Zorko Aleksandr <alexzorko@yandex.ru>
2023-02-01 00:24:11 +0200

    1. В единой точке входа реализована автозагрузка по стандарту PSR4 2. Класс Application теперь абстрактный 3. При запуске команды Application::run() автозагрузка будет подключена автоматически.

--- src/Application.php
+++ src/Application.php
@@ -2,10 +2,35 @@
 
 namespace App;
 
-class Application
+abstract class Application
 {
-	public function run()
+	public static function run(): void
 	{
+		self::autoload();
+	}
 
+	public static function autoload(): void
+	{
+		spl_autoload_register(static function($class) {
+			$prefix = "App\\";
+			$base_dir = __DIR__ . '/';
+
+			$len = strlen($prefix);
+			if (strncmp($prefix, $class, $len) !== 0)
+			{
+				return;
+			}
+
+			$relative_class = substr($class, $len);
+
+			$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+			if (file_exists($file))
+			{
+				require_once $file;
+			}
+		});
 	}
 }
+
+Application::run();

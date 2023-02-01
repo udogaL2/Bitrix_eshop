25b2fa46e856f8cc24adac4bcd01470e085c35b6
Valeria <valka.tga@gmail.com>
2023-02-01 17:04:10 +0200

    Шаблон детальной страницы товара

---
+++ src/View/Detail/index.html.php
@@ -0,0 +1,8 @@
+<div class="detail-list">
+	<div class="detail-list-title">Title of Good</div>
+	<div class="detail-list-img">Image of Good</div>
+	<div class="detail-list-description">Description of Good</div>
+	<div class="detail-list-producer">Producer of Good</div>
+	<div class="detail-list-price">Price of Good</div>
+	<div class="tag-good">Tag of Good</div>
+</div>

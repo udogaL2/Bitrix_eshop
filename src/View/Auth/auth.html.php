b802b22cc8ee2a736de0ab28a3fa5284826d42f0
Valeria <valka.tga@gmail.com>
2023-02-08 19:31:29 +0200

    Добавлены шаблон для авторизации администратора

---
+++ src/View/Auth/auth.html.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href="../reset.css" rel="stylesheet">
+	<link href="../style.css" rel="stylesheet">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title></title>
+</head>
+<body>
+
+<div class="content-auth">
+	<div class="content-auth-title">Авторизация</div>
+	<div class="div-content-auth-login">
+		<label>
+			<input type="text" class="content-auth-login" placeholder="Введите имя" minlength="3" maxlength="15" required>
+		</label>
+		<span class="validity"></span>
+	</div>
+	<div class="div-content-auth-password">
+		<label>
+			<input type="text" class="content-auth-password" placeholder="Введите фамилию" minlength="3" maxlength="15" required>
+		</label>
+		<span class="validity"></span>
+	</div>
+	<a class="content-auth-button" href="#">Войти</a>
+</div>
+
+</body>
+</html>

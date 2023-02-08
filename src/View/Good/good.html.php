b7b96b4e8611dcf3747a910089aaa5c29c90960f
Valeria <valka.tga@gmail.com>
2023-02-08 19:28:07 +0200

    Добавлены стили в шаблон товаров

--- src/View/Good/good.html.php
+++ src/View/Good/good.html.php
@@ -1,7 +1,18 @@
 <?php /** @var App\Src\Model\Good[] $goods */ ?>
 
-<div class="grid text-center">
-	<div class="row ">
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href="../reset.css" rel="stylesheet">
+	<link href="../style.css" rel="stylesheet">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title></title>
+</head>
+<body>
+
+<div class="content">
 		<?php foreach($goods as $good): ?>
 			<div class="goods-item">
 				<div class="img-good"></div>
@@ -17,5 +28,6 @@
 <!--				</div>-->
 			</div>
 		<?php endforeach; ?>
-	</div>
 </div>
+</body>
+</html>

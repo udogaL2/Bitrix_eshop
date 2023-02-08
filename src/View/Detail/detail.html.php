fb4d984a5e5167a9c9b051da4b2e2d8bd5a667eb
Valeria <valka.tga@gmail.com>
2023-02-08 19:27:30 +0200

    Добавлены стили в  шаблон детальной страницы

---
+++ src/View/Detail/detail.html.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href="../reset.css" rel="stylesheet">
+	<link href="../style.css" rel="stylesheet">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title></title>
+</head>
+<body>
+<div class="detail-back">
+	<a href="#" class="content-detail-back"></a>
+</div>
+<div class="content-detail">
+	<div class="detail-list">
+		<div class="detail-list-img"></div>
+		<div class="detail-list-title">Title of Good</div>
+		<div class="detail-list-description">Description</div>
+		<div class="detail-list-producer">Producer of Good</div>
+		<div class="detail-list-tag">Tag of Good</div>
+	</div>
+	<div class="detail-price-buy">
+		<div class="detail-price">500 RUB</div>
+		<a href="" class="detail-button-buy">Buy</a>
+	</div>
+
+</div>
+</body>
+</html>
+
+

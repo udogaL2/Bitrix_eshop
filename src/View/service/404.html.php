6cba58865d2a17d7c1a0652b7c60f105c93021bd
Valeria <valka.tga@gmail.com>
2023-02-08 19:17:07 +0200

    Добавлены стили в шаблон 404

--- src/View/service/404.html.php
+++ src/View/service/404.html.php
@@ -4,9 +4,11 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="../reset.css" rel="stylesheet">
+	<link href="../style.css" rel="stylesheet">
 	<title>Not Found</title>
 </head>
 <body>
-	<p>404: Page not found</p>
+<p class="page-not-found">404: Page not found</p>
 </body>
 </html>

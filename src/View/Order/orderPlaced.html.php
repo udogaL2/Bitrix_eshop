d32d7526983d2d0a98cf77e061db02918cb4107c
Valeria <valka.tga@gmail.com>
2023-02-08 19:35:39 +0200

    Добавлен шаблон оформленного заказа

---
+++ src/View/Order/orderPlaced.html.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href="../reset.css" rel="stylesheet">
+	<link href="../style.css" rel="stylesheet">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title></title>
+</head>
+<body>
+
+<div class="content-order-placed">
+	<div class="content-order-placed-icon"></div>
+	<div class="content-order-placed-title">
+		Ваш заказ оформлен
+	</div>
+</div>
+
+</body>
+</html>

1db97c5da472fff78b23fce7c056af5a29e96a99
valer <valka.tga@gmail.com>
2023-02-17 00:28:47 +0200

    Добавлены стили для главной страницы с шапкой и футером

--- src/View/Main/index.html.php
+++ src/View/Main/index.html.php
@@ -7,8 +7,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link href="../../public/reset.css" rel="stylesheet">
-    <link href="../../public/style.css" rel="stylesheet">
+    <link href="/public/reset.css" rel="stylesheet">
+    <link href="/public/style.css" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title></title>
 </head>
@@ -20,7 +20,7 @@
 		</a>
 
         <div class="div-search">
-            <div class="icon-search"></div>
+            <img src="/src/View/icons/icon-search.svg" class="icon-search"></img>
             <label>
                 <input type="text" class="search" placeholder="Поиск по товарам">
             </label>
@@ -43,8 +43,6 @@
         </div>
         <div class="follow">
             <p class="follow-item">Будьте с нами</p>
-            <div class="follow-icon-facebook"></div>
-            <a class="follow-item">Facebook</a>
             <div class="follow-icon-twitter"></div>
             <a class="follow-item">Telegram</a>
             <div class="follow-icon-gmail"></div>

a3ba5d7e435347021b6847c3789522ea6ca1eddd
valer <valka.tga@gmail.com>
2023-02-16 10:20:19 +0200

    Добалены некоторые стили для главной страницы, сверстана поисковая строка

--- src/View/Good/good.html.php
+++ src/View/Good/good.html.php
@@ -6,16 +6,18 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link href="/reset.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 	<link href="/style.css" rel="stylesheet">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title></title>
 </head>
 <body>
 
-<div class="content">
+<div class="container text-center">
+<div class="content row row-cols-3">
 		<?php foreach($goods as $good): ?>
-			<a href="/product/<?=$good->getId()?>">
-			<div class="goods-item">
+			<a class="goods-item" href="/product/<?=$good->getId()?>">
+			<div>
 				<img class="img-good" src="/<?= $good->getImages()[0]->getPath() ?>">
 				<div class="title-good">Good №<?= $good->getId() ?></div>
                 <div class="title-good">Good Name:<?= $good->getName() ?></div>
@@ -31,6 +33,7 @@
 			</div>
 			</a>
 		<?php endforeach; ?>
+</div>
 </div>
 </body>
 </html>
--- src/View/Main/index.html.php
+++ src/View/Main/index.html.php
@@ -5,28 +5,50 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-	<link href="/reset.css" rel="stylesheet">
-	<link href="/style.css" rel="stylesheet">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="../../public/reset.css" rel="stylesheet">
+    <link href="../../public/style.css" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
 </head>
 <body>
-	<header>
-		<div class="header">
-			<p></p>
-		</div>
-	</header>
+<header>
+    <div class="header">
 
-	<div class="content">
-		<?= $content ?>
-	</div>
+        <div class="logo-fon"><div class="logo"></div></div>
+        <div class="div-search">
+            <div class="icon-search"></div>
+            <label>
+                <input type="text" class="search" placeholder="Поиск по товарам">
+            </label>
+            <a class="button-search" href="#">Поиск</a>
+        </div>
 
-	<footer>
-		<div class="footer">
-		</div>
-	</footer>
+    </div>
+</header>
+
+<div class="content">
+    <?= $content ?>
+</div>
+
+<footer>
+    <div class="footer">
+        <div class="about">
+            <p class="about-item">О компании</p>
+            <a class="about-item">О нас</a>
+            <a class="about-item">Отзывы</a>
+        </div>
+        <div class="follow">
+            <p class="follow-item">Будьте с нами</p>
+            <div class="follow-icon-facebook"></div>
+            <a class="follow-item">Facebook</a>
+            <div class="follow-icon-twitter"></div>
+            <a class="follow-item">Telegram</a>
+            <div class="follow-icon-gmail"></div>
+            <a class="follow-item">gmail</a>
+        </div>
+    </div>
+</footer>
 </body>
 </html>

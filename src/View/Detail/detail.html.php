cb4631e8f9606bef80290ffefab919b3168a6891
valer <valka.tga@gmail.com>
2023-02-26 23:50:45 +0200

    Добавлена карусель картиночек

--- src/View/Detail/detail.html.php
+++ src/View/Detail/detail.html.php
@@ -1,4 +1,4 @@
-<?php /** @var App\Src\Model\Good $good */ ?>
+<?php /** @var App\Src\Model\Good $good */?>
 
 <!doctype html>
 <html lang="en">
@@ -8,14 +8,34 @@
 	<link href="/reset.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 	<link href="/style.css" rel="stylesheet">
+<!--    <link href="/DetailStyle.css" rel="stylesheet">-->
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title></title>
 </head>
 <body>
-
 <div class="content-detail">
 	<div class="detail-list">
-		<img class="detail-list-img" src="/<?= $good->getImages()[0]->getPath() ?>">
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="/<?= $good->getImages()[0]->getPath() ?>" class="d-block w-100 detail-list-img" alt="...">
+                </div>
+                <?php foreach (array_slice($good->getImages(),1) as $item):?>
+
+                <div class="carousel-item">
+                    <img src="/<?= $item->getPath() ?>" class="d-block w-100 detail-list-img" alt="...">
+                </div>
+                <?php endforeach?>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
         <div class="detail-text">
             <div class="detail-list-title"><?= $good->getName() ?></div>
             <div class="detail-list-description"><?= $good->getFullDesc() ?></div>
@@ -31,8 +51,9 @@
 		<div class="detail-price"><?= $good->getPrice() ?> ₽</div>
 		<a href="/order/<?= $good->getId() ?>" class="detail-button-buy">Buy</a>
 	</div>
-
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 </body>
 </html>
 
